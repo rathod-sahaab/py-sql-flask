147c5cfddad2e50b71abe8d2d2b8e89921012e8d
Abhay Raj Singh <abhay@tealfeed.com>
2022-05-06 00:46:31 +0530

    any null field countries

--- task-1/sql/p09-any-null-countries.sql
+++ task-1/sql/p09-any-null-countries.sql
@@ -1,1 +1,10 @@
--- TODO
+SELECT count(*) as "Null Field Countries" FROM country
+WHERE IndepYear IS NULL OR LifeExpectancy IS NULL OR GNP IS NULL OR HeadOfState IS NULL OR Capital IS NULL;
+
+/*
++----------------------+
+| Null Field Countries |
++----------------------+
+|                   49 |
++----------------------+
+*/
